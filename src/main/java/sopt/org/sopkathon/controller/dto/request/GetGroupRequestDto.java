c365baa45b727309b74c3652188d87e164846270
jun02160 <jun020216@naver.com>
2023-05-21 02:18:30 +0900

    [FIX] SQL 예약어 충돌 해결 #2

--- src/main/java/sopt/org/sopkathon/controller/dto/request/GetGroupRequestDto.java
+++ src/main/java/sopt/org/sopkathon/controller/dto/request/GetGroupRequestDto.java
@@ -3,9 +3,11 @@ package sopt.org.sopkathon.controller.dto.request;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class GetGroupRequestDto {
 
     private Long groupId;
