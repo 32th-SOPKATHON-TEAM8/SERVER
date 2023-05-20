3b47c345a44b625e37d6524e95c722aeb0ec7a9b
sunseo18 <sunseo1818@gmail.com>
2023-05-21 03:46:43 +0900

    [CHORE] lint 적용 #4

--- src/main/java/sopt/org/sopkathon/infrastructure/UserRepository.java
+++ src/main/java/sopt/org/sopkathon/infrastructure/UserRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.repository.Repository;
 import sopt.org.sopkathon.domain.User;
 
 public interface UserRepository extends Repository<User, Long> {
+
     Optional<User> findById(Long id);
+
     void save(User user);
 }
--- src/main/java/sopt/org/sopkathon/service/UserService.java
+++ src/main/java/sopt/org/sopkathon/service/UserService.java
@@ -4,19 +4,21 @@ import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sopt.org.sopkathon.domain.User;
+import sopt.org.sopkathon.exception.Error;
 import sopt.org.sopkathon.exception.model.NotFoundException;
 import sopt.org.sopkathon.infrastructure.UserRepository;
-import sopt.org.sopkathon.exception.Error;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
     private final UserRepository userRepository;
 
     @Transactional
     public void report(final Long userId) {
         User user = userRepository.findById(userId)
-            .orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_USER_EXCEPTION, Error.NOT_FOUND_USER_EXCEPTION.getMessage()));
+            .orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_USER_EXCEPTION,
+                Error.NOT_FOUND_USER_EXCEPTION.getMessage()));
 
         user.reported();
         userRepository.save(user);
