c0b391abde6488efedb7dc56c3b0b5b08b26d6cd
sunseo18 <sunseo1818@gmail.com>
2023-05-21 00:25:25 +0900

    [FEAT] entity 구현 #1

---
+++ src/main/java/sopt/org/sopkathon/domain/User.java
@@ -0,0 +1,32 @@
+package sopt.org.sopkathon.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private int life;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="group_id")
+    private Group group;
+}
